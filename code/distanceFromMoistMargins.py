208da7c9243b055a01e3a30f892066eb8df1efb3
Julia Windmiller <julia.windmiller@mpimet.mpg.de>
2025-09-11 15:43:53 +0200

    Cleans up current version of edge finding routine

--- code/distanceFromMoistMargins.py
+++ code/distanceFromMoistMargins.py
@@ -1,20 +1,32 @@
-#%%    
+# %%
+import sys
+
+sys.path.append("./code")
+
+# %%
 
 import cartopy.crs as ccrs
 import xarray as xr
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sb
-from edgeFinder import find_edges_numpy
 from plotUtils import plot_cwv_field, add_east_west_boxes
 
+import importlib
+import scipy
+import edgeFinder
 
-#%%
+importlib.reload(edgeFinder)
+from edgeFinder import find_edges_numpy
+
+# %%
 cwv_thresh = 50
 cwv_min = 45
-#%%
+# %%
 
-data_path = "/Users/juliawindmiller/MPI/Windmiller2025_ObservingVerticalVelocities/data/"
+data_path = (
+    "/Users/juliawindmiller/MPI/Windmiller2025_ObservingVerticalVelocities/data/"
+)
 file_name = "msk_tcwv-2024-08-09-1hr_22_1_-61_-19.nc"
 cwv_orcestra = xr.open_dataset(data_path + file_name)
 # %%
@@ -26,18 +38,17 @@ cwv_max_lat = cwv_orcestra_mean.idxmax("latitude")
 cwv_max_lat_smooth = cwv_max_lat.rolling(longitude=10, center=True).mean()
 
 
-#%%
+# %%
 
 latitudes_np = cwv_orcestra_mean.latitude.values
 
 results = xr.apply_ufunc(
     find_edges_numpy,
-    cwv_orcestra_mean,          # (latitude, longitude)
-    cwv_max_lat_smooth,         # (longitude)
+    cwv_orcestra_mean,  # (latitude, longitude)
+    cwv_max_lat_smooth,  # (longitude)
     kwargs={
         "latitudes": latitudes_np,
         "cwv_thresh": cwv_thresh,
-        "cwv_min": 0,
     },
     input_core_dims=[["latitude"], []],
     output_core_dims=[["edge_type"]],
@@ -54,25 +65,28 @@ extent = [-65, -15, -5, 25]
 ax = plot_cwv_field(cwv_orcestra_mean, levels=levels_cwv, extent=extent)
 add_east_west_boxes(ax)
 
-plt.scatter(cwv_max_lat_smooth.longitude, cwv_max_lat_smooth, label="Latitude of max. CWV (smoothed)")
+plt.scatter(
+    cwv_max_lat_smooth.longitude,
+    cwv_max_lat_smooth,
+    label="Latitude of max. CWV (smoothed)",
+)
 plt.scatter(results.longitude, results.sel(edge_type=0), label="Southern edge")
 plt.scatter(results.longitude, results.sel(edge_type=1), label="Northern edge")
 plt.legend()
 
 sb.despine()
 
-plt.savefig("../figures/cwvMean.pdf", bbox_inches = "tight")
+# plt.savefig("../figures/cwvMean.pdf", bbox_inches="tight")
 
 # %%
 
 all_time_results = xr.apply_ufunc(
     find_edges_numpy,
-    cwv_orcestra.tcwv,          # (latitude, longitude)
-    cwv_max_lat_smooth,         # (longitude)
+    cwv_orcestra.tcwv,  # (latitude, longitude)
+    cwv_max_lat_smooth,  # (longitude)
     kwargs={
         "latitudes": latitudes_np,
         "cwv_thresh": cwv_thresh,
-        "cwv_min": 0,
     },
     input_core_dims=[["latitude"], []],
     output_core_dims=[["edge_type"]],
@@ -85,20 +99,35 @@ all_time_results.compute()
 
 # %%
 
-time_ind = np.random.randint(len(cwv_orcestra.time))
+time_ind = 77  # np.random.randint(len(cwv_orcestra.time))
 
-test = cwv_orcestra.tcwv.isel(time=time_ind)
+test = cwv_orcestra.tcwv.isel(time=time_ind).compute()
 
 ax = plot_cwv_field(test, levels=levels_cwv, extent=extent)
 add_east_west_boxes(ax)
 
 test_results = all_time_results.isel(time=time_ind)
 
-plt.scatter(cwv_max_lat_smooth.longitude, cwv_max_lat_smooth, label="Latitude of max. CWV (smoothed)")
-plt.scatter(test_results.longitude, test_results.sel(edge_type=0), label="Southern edge")
-plt.scatter(test_results.longitude, test_results.sel(edge_type=1), label="Northern edge")
+plt.scatter(
+    cwv_max_lat_smooth.longitude,
+    cwv_max_lat_smooth,
+    label="Latitude of max. CWV (smoothed)",
+)
+plt.scatter(
+    test_results.longitude, test_results.sel(edge_type=0), label="Southern edge"
+)
+plt.scatter(
+    test_results.longitude, test_results.sel(edge_type=1), label="Northern edge"
+)
 plt.legend()
+
 # %%
 
+test_north = test_results.sel(edge_type=0).dropna(dim="longitude")
 
+spl, u = scipy.interpolate.make_splprep([test_north.longitude, test_north], s=5)
+
+ax = plot_cwv_field(test, levels=levels_cwv, extent=extent)
+add_east_west_boxes(ax)
+ax.plot(spl(u)[0] - 360, spl(u)[1])
 # %%
--- code/edgeFinder.py
+++ code/edgeFinder.py
@@ -1,13 +1,20 @@
 import numpy as np
 import scipy.signal
 
-def find_edges_numpy(cwv, lat_cwv_max, latitudes, cwv_thresh=45, cwv_min=0):
+
+def find_edges_numpy(cwv, lat_cwv_max, latitudes, cwv_thresh=45):
     """
+    Detect northern and southern moist margin as defined by CWV = cwv_thresh.
+    To distinguish between the northern and southern moist margin,
+    first determine the latitude of maximum CWV, larger or equal than cwv_thresh.
+
     Parameters:
         cwv (np.ndarray): 1D array of CWV values.
         lat_cwv_max (float): Latitude of CWV peak in the mean profile.
         latitudes (np.ndarray): 1D array of latitudes (same length as cwv).
+        cwv_thresh (float): CWV of moist margin.
     """
+
     if np.max(cwv) <= cwv_thresh:
         return np.array([np.nan, np.nan], dtype=np.float32)
 
@@ -19,16 +26,22 @@ def find_edges_numpy(cwv, lat_cwv_max, latitudes, cwv_thresh=45, cwv_min=0):
     closest_peak_idx = np.argmin(np.abs(peak_lats - lat_cwv_max))
     lat_peak = peak_lats[closest_peak_idx]
 
-    moist_mask = cwv > cwv_min
-
-    north_mask = (latitudes >= lat_peak) & moist_mask
+    north_mask = latitudes >= lat_peak
     north_cwv = cwv[north_mask]
     north_lats = latitudes[north_mask]
-    lat_north = np.min(north_lats[north_cwv <= cwv_thresh]) if np.any(north_cwv <= cwv_thresh) else np.nan
+    lat_north = (
+        np.min(north_lats[north_cwv <= cwv_thresh])
+        if np.any(north_cwv <= cwv_thresh)
+        else np.nan
+    )
 
-    south_mask = (latitudes <= lat_peak) & moist_mask
+    south_mask = latitudes <= lat_peak
     south_cwv = cwv[south_mask]
     south_lats = latitudes[south_mask]
-    lat_south = np.min(south_lats[south_cwv >= cwv_thresh]) if np.any(south_cwv >= cwv_thresh) else np.nan
+    lat_south = (
+        np.max(south_lats[south_cwv <= cwv_thresh])
+        if np.any(south_cwv <= cwv_thresh)
+        else np.nan
+    )
 
     return np.array([lat_south, lat_north], dtype=np.float32)
