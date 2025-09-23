91555e4b6bf7acfb49b81a92b029b03368b844ce
Julia Windmiller <julia.windmiller@mpimet.mpg.de>
2025-09-23 10:37:25 +0200

    Changes binary field to simple thresholded field

--- code/cwvMarginLength.py
+++ code/cwvMarginLength.py
@@ -7,63 +7,87 @@ from skimage.measure import find_contours
 
 # %%
 
-data_path = "../data/"
-file_name = "orcestra_era5_tcwv"
-tcwv_cluster = xr.open_dataset(data_path + file_name + ".nc")
+data_path = (
+    "/Users/juliawindmiller/MPI/Windmiller2025_ObservingVerticalVelocities/data/"
+)
 
-# %%
-
-largest_cluster_pad = tcwv_cluster.largest_cluster
-largest_cluster_pad[:, 0, :] = 0
-largest_cluster_pad[:, -1, :] = 0
-
-largest_cluster_pad = largest_cluster_pad.where(~np.isnan(largest_cluster_pad), 0)
+file_name = "msk_tcwv-2024-08-09-1hr_22_1_-61_-19.nc"
+cwv_orcestra = xr.open_dataset(data_path + file_name)
+time_da = cwv_orcestra.time
 
 res_in_deg = 0.25
-cwv_thresh = 50
+cwv_thresh = 48
+min_contour_length = 6.0
 
 # %%
 
 
-def contour_length(contours, res):
+def cluster_field_with_border(da, thresh):
+    field_with_border = xr.where(da > thresh, 1, 0)
+    field_with_border[:, 0, :] = 0
+    field_with_border[:, -1, :] = 0
+    return field_with_border
+
+
+def contour_length(contours, res, min_contour_length):
     cl = 0
     for c in range(len(contours)):
         delta_x = contours[c][:, 1][1:] - contours[c][:, 1][:-1]
         delta_y = contours[c][:, 0][1:] - contours[c][:, 0][:-1]
-        cl += np.sum(np.sqrt(delta_x**2 + delta_y**2)) * res
+        length_c = np.sum(np.sqrt(delta_x**2 + delta_y**2)) * res
+        if length_c >= min_contour_length:
+            cl += length_c
     return cl
+
+
+def ref_length(binary_field):
+    i_contour_lon = binary_field.mean("latitude")
+    return i_contour_lon.where(i_contour_lon > 0, drop=True).count() * 2 * res_in_deg
+
+
+def calc_blw_ds(cluster_fields, res_in_deg, min_contour_length):
+
+    times_ds = cluster_fields.time
+    for i_time in range(len(times_ds)):
+
+        cluster_fields_ts = cluster_fields.isel(time=i_time)
+
+        contour_lengths[i_time] = contour_length(
+            find_contours(cluster_fields_ts.values, level=0.5),
+            res_in_deg,
+            min_contour_length,
+        )
+
+        contour_refs[i_time] = ref_length(cluster_fields_ts)
+
+    contour_ds = xr.Dataset(
+        data_vars=dict(
+            contour_lengths=(["time"], contour_lengths),
+            contour_refs=(["time"], contour_refs),
+        ),
+        coords=dict(time=times_ds),
+    )
+
+    contour_ds["blw"] = contour_ds["contour_refs"] / contour_ds["contour_lengths"]
+
+    return contour_ds
 
 
 # %%
 
-contour_lengths = np.empty(len(largest_cluster_pad.time))
-contour_refs = np.empty(len(largest_cluster_pad.time))
+contour_lengths = np.empty(len(time_da))
+contour_refs = np.empty(len(time_da))
 
+cluster_fields = cluster_field_with_border(cwv_orcestra.tcwv, cwv_thresh)
+cluster_fields_sens = cluster_field_with_border(cwv_orcestra.tcwv, 45)
 
-for i_time in range(len(largest_cluster_pad.time)):
-    largest_cluster_pad_ts = largest_cluster_pad.isel(time=i_time)
+contour_ds = calc_blw_ds(cluster_fields, res_in_deg, min_contour_length)
+contour_ds_sens = calc_blw_ds(cluster_fields_sens, res_in_deg, min_contour_length)
 
-    i_contour_ds = largest_cluster_pad_ts
+# %%
 
-    i_contours = find_contours(i_contour_ds.values)
-    contour_lengths[i_time] = contour_length(i_contours, res_in_deg)
-
-    i_contour_lon = i_contour_ds.mean("latitude")
-    contour_refs[i_time] = (
-        i_contour_lon.where(i_contour_lon > 0, drop=True).count() * 2 * res_in_deg
-    )
-
-
-contour_ds = xr.Dataset(
-    data_vars=dict(
-        contour_lengths=(["time"], contour_lengths),
-        contour_refs=(["time"], contour_refs),
-    ),
-    coords=dict(time=largest_cluster_pad.time),
-)
-
-contour_ds["blw"] = contour_ds["contour_refs"] / contour_ds["contour_lengths"]
-
+contour_ds.blw.plot()
+contour_ds_sens.blw.plot()
 # %%
 
 # tcwv_cluster = xr.merge([tcwv_cluster, contour_ds])
@@ -75,26 +99,35 @@ contour_ds["blw"].plot()
 
 # %%
 # Example
-time = "2024-08-28T19:00:00.000000000"  # "2024-09-07T15"
-largest_cluster_pad_ts = largest_cluster_pad.sel(time=time)
+time = "2024-08-15T12:00:00.000000000"  # "2024-09-07T15"
+cluster_fields_ts = cluster_fields.sel(time=time)
 
-test_contour_ds = largest_cluster_pad_ts
-test_contours = find_contours(test_contour_ds.values)
+test_contour_ds = cluster_fields_ts
+test_contours = find_contours(test_contour_ds.values, level=0.5)
 
 fig, ax = plt.subplots(1, 1)
 plt.sca(ax)
 
 plt.imshow(
-    tcwv_cluster.tcwv.sel(time=time).values[::-1, :], origin="lower", cmap="Blues"
+    cluster_fields.sel(time=time).values[::-1, :],
+    origin="lower",
+    cmap="Blues",
+    vmin=46,
+    vmax=64,
 )
 
 for c in range(len(test_contours)):
 
-    plt.scatter(
-        test_contours[c][:, 1], np.shape(tcwv_cluster.tcwv)[1] - test_contours[c][:, 0]
-    )
-    print(contour_length([test_contours[c]], res_in_deg))
+    c_length = contour_length([test_contours[c]], res_in_deg, min_contour_length)
 
-print(contour_length(test_contours, res_in_deg))
+    if c_length >= 3.0:
+
+        plt.scatter(
+            test_contours[c][:, 1],
+            np.shape(cluster_fields)[1] - test_contours[c][:, 0],
+        )
+        print(c_length)
+
+print(contour_length(test_contours, res_in_deg, min_contour_length))
 
 # %%
